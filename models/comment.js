3f97e6c5e9d5a13245289a7081da5373066c7829
saulwade <saulwade29@gmail.com>
2023-03-28 10:10:22 -0600

    added all the javascript code for the models folder

--- models/comment.js
+++ models/comment.js
@@ -1,19 +1,47 @@
+// models/Comment.js
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 class Comment extends Model {}
 
 Comment.init(
-  {
-    // Define comment fields
-  },
-  {
-    sequelize,
-    timestamps: true,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'comment',
-  }
-);
-
-module.exports = Comment;
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      text: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'user',
+          key: 'id',
+        },
+      },
+      post_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'post',
+          key: 'id',
+        },
+      },
+    },
+    {
+      sequelize,
+      timestamps: true,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'comment',
+    }
+  );
+  
+  module.exports = Comment;
+  
+     
