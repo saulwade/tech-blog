3198d4c2b6eb662e642943111babd26bf17a7213
saulwade <saulwade29@gmail.com>
2023-03-28 10:13:01 -0600

    added the post.js code

--- models/post.js
+++ models/post.js
@@ -5,7 +5,27 @@ class Post extends Model {}
 
 Post.init(
   {
-    // Define post fields
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
