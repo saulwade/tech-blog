98640a7221e23211461ae475e3bf10864033153e
saulwade <saulwade29@gmail.com>
2023-03-29 21:57:42 -0600

    solved some bugs

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,12 +1,20 @@
 function checkAuthentication(req, res, next) {
-    if (req.session && req.session.user_id) {
-      next();
-    } else {
-      res.status(401).json({ message: 'You must be logged in to perform this action' });
-    }
+  if (req.session && req.session.user_id) {
+    next();
+  } else {
+    res.status(401).json({ message: 'You must be logged in to perform this action' });
   }
-  
-  module.exports = {
-    checkAuthentication,
-  };
-  
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (!req.session || !req.session.user_id) {
+    next();
+  } else {
+    res.status(401).json({ message: 'You are already logged in' });
+  }
+}
+
+module.exports = {
+  checkAuthentication,
+  checkNotAuthenticated,
+};
--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -2,14 +2,14 @@ const { Comment } = require('../models');
 
 const commentData = [
   {
-    content: 'This is a sample comment.',
-    user_id: 1, 
-    post_id: 1, 
+    text: 'This is a comment from user 1',
+    user_id: 1,
+    post_id: 1,
   },
   {
-    content: 'This is another sample comment.',
-    user_id: 2, 
-    post_id: 1, 
+    text: 'This is a comment from user 2',
+    user_id: 2,
+    post_id: 1,
   },
 ];
 
